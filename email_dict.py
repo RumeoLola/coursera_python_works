5c571dcda9247bd4f092734478c12205a6f52976
RumeoLola <rumeomololaz@gmail.com>
2022-05-21 09:53:22 -0400

    remade the display email with a dictionary

---
+++ email_dict.py
@@ -0,0 +1,23 @@
+# test again thebox.txt
+
+
+name = input("Enter file: ")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+dice = {}
+count = 0
+for line in handle:
+    line = line.rstrip()
+    line = line.split()
+    if "From" in line:
+        count += 1
+        for word in line:
+            # get each email
+            dice[word] = dice.get(word, 0) + 1
+    else:
+        continue
+    print(*line[1:2])
+print("There were", count, "lines in the file with From as the first word")
+handle.close()
